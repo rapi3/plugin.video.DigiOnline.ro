69fb3c3cb07752c750c05ebfb62fc68451d8f476
cralin <cralin@gmail.com>
2020-02-24 21:11:32 +0100

    Setup the stage

---
+++ resources/lib/common.py
@@ -0,0 +1,54 @@
+#
+#
+#    Copyright (C) 2020  Alin Cretu
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
+
+def get_cached_categories():
+    ####
+    #
+    # Get the list of cached video categories.
+    #
+    # Return: The list of cached video categories
+    #
+    ####
+
+    logger.debug('Enter function')
+
+    logger.debug('Exit function')
+    return _cached_categories_list_
+
+
+def get_cached_channels(category)
+    ####
+    #
+    # Get the cached list of channels/streams.
+    #
+    # Parameters:
+    #      category: Category name
+    #
+    # Return: The list of cached channels/streams in the given category
+    #
+    ####
+
+    logger.debug('Enter function')
+
+    logger.debug('Exit function')
+    return _cached_channels_list_
+
+
+
